9f44a882854616a72989ad62a0241daa6fcd1e5f
dnv26 <55224281+dnv26@users.noreply.github.com>
2019-10-15 17:39:56 -0400

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -273,7 +273,7 @@ class CarInterface(object):
 
       
     events = []
-    if not ret.gearShifter == GearShifter.drive:
+    if ret.gearShifter == GearShifter.drive:
       events.append(create_event('pcmEnable', [ET.ENABLE]]))
 #    if ret.doorOpen:
 #      events.append(create_event('doorOpen', [ET.NO_ENTRY, ET.SOFT_DISABLE]))
