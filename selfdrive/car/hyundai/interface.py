aa6b4dd1539f99d6253ceed9d24a541ed6d1cf78
dnv26 <55224281+dnv26@users.noreply.github.com>
2019-10-15 18:07:08 -0400

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -56,7 +56,7 @@ class CarInterface(object):
     ret.steerActuatorDelay = 0.1  # Default delay
     ret.steerRateCost = 0.5
     
-    tire_stiffness_factor = 1.
+    tire_stiffness_factor = 0.8
 
     ret.minEnableSpeed = -1.   # enable is done by stock ACC, so ignore this
     
@@ -88,12 +88,12 @@ class CarInterface(object):
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.25], [0.05]]
       ret.minSteerSpeed = 32 * CV.MPH_TO_MS
     elif candidate == CAR.GENESIS:
-      ret.lateralTuning.pid.kf = 0.00005
+      ret.lateralTuning.pid.kf = 0.00014
       ret.mass = 2060. + STD_CARGO_KG
       ret.wheelbase = 3.01
-      ret.steerRatio = 11.6
+      ret.steerRatio = 11.8
       ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.16], [0.01]]
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.22], [0.09]]
       ret.minSteerSpeed = 49 * CV.KPH_TO_MS
       ret.minEnableSpeed = 54 * CV.KPH_TO_MS
     elif candidate in [CAR.GENESIS_G90, CAR.GENESIS_G80]:
