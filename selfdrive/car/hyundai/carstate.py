6384cfaa24f551b41630bd1616db8c0ac897a430
dnv26 <55224281+dnv26@users.noreply.github.com>
2019-10-17 19:43:04 -0400

    revert revert

--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -166,7 +166,7 @@ class CarState(object):
     self.main_on = (cp.vl["SCC11"]["MainMode_ACC"] != 0) if self.has_scc else \
                                             cp.vl['EMS16']['CRUISE_LAMP_M']
     self.acc_active = (cp.vl["SCC12"]['ACCMode'] != 0) if self.has_scc else \
-                                       (cp.vl["LVR12"]['CF_Lvr_CruiseSet'] != 0)
+                                      self.main_on
     self.pcm_acc_status = int(self.acc_active)
 
     # calc best v_ego estimate, by averaging two opposite corners
