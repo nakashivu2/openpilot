b316314410cf2121d4c25e6a219f7f7f8fcd86de
dnv26 <55224281+dnv26@users.noreply.github.com>
2019-10-16 22:16:01 -0400

    bug

--- panda/board/safety/safety_hyundai.h
+++ panda/board/safety/safety_hyundai.h
@@ -55,7 +55,7 @@ static void hyundai_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
   // cruise control for car without SCC
   if ((addr == 832) && (!hyundai_has_scc)) {
     // first byte
-    int cruise_engaged = 1
+    int cruise_engaged = 1;
     //if (cruise_engaged && !hyundai_cruise_engaged_last) {
       controls_allowed = 1;
     //}
