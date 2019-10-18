c72bd79a6f1e625d909284b5d4f18c243b3ad743
dnv26 <55224281+dnv26@users.noreply.github.com>
2019-10-18 08:05:58 -0400

    Update carcontroller.py

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -50,7 +50,7 @@ class CarController(object):
   def update(self, enabled, CS, frame, actuators, pcm_cancel_cmd, hud_alert,
               left_line, right_line, left_lane_depart, right_lane_depart):
 
-    if CS.left_blinker_on or CS.right_blinker_on:
+    if CS.left_blinker_flash or CS.right_blinker_flash:
       self.turning_signal_timer = 100  # Disable for 1.0 Seconds after blinker turned off
     if self.turning_signal_timer:
       enabled = 0
